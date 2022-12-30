62d039f8fbf99dde83adab0619a715edf5e98616
codewithchinekwe <codewithchinekwe@gmail.com>
2022-12-30 21:45:27 +0100

    added form_validation

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -13,6 +13,10 @@ class LoginController extends Controller
 
     public function loginSubmit(Request $request)
     {
+        $validatedData = $request->validate([
+            'email' => 'required | email',
+            'password' => 'required | min:6 | max:12'
+        ]);
         $email = $request->input('email');
         $password = $request->input('password');
         return "Email is " . " " . $email . " Password is " . $password;

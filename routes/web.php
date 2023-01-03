9eb1132c90628554dfc69807b3aae1f41ed31a2c
chinekwe_clever <chinekwe_clever@outlook.com>
2023-01-02 18:07:47 -0800

    added blade template layout

--- routes/web.php
+++ routes/web.php
@@ -55,3 +55,15 @@ Route::get('/get-all-posts', [PostController::class, 'getAllPostsUsingModel'])->
 Route::get('/test', function () {
     return view('test');
 });
+
+Route::get('/home', function(){
+    return view('index');
+});
+
+Route::get('/about', function(){
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
